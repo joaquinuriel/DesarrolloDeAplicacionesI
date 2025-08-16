52dd4ecde9a2e501fe8bf51f917fe78ed2db1224
Joaquín <joaquinuriel@icloud.com>
2025-08-16 12:58:28 -0300

    Creación del proyecto

---
+++ app/src/main/java/edu/uade/ritmofit/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.uade.ritmofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
